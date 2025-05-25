fbca76473741c062cb146865b8e8e2cde1a565cf
rainakallan <rainakallan@outlook.com>
2025-05-26 02:20:46 +0530

    First Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import { Hero } from "../components/Hero";
 import { About } from "../components/About";
 import { Skills } from "../components/Skills";
-import { Projects } from "../components/projects";
+import { Projects } from "../components/Projects";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
